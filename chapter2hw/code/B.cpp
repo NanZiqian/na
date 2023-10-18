b56bc2f682fb2648af1589724fff815b5238dd66
obersturrrm <1727411859@qq.com>
2023-10-18 12:24:21 +0800

    10.18 chapter2 progress

--- chapter2hw/code/B.cpp
+++ chapter2hw/code/B.cpp
@@ -1,4 +1,5 @@
 #include"Newton interpolation.h"
+#include<fstream>
 
 using namespace std;
 
@@ -10,16 +11,25 @@ class function1:public Function{
 };
 
 int main(){
-    vector<double> vector_x;
-    function1 f;
+    vector<double> vector_x,parameters;
+    function1 *f = new function1() ;
     NewtonInterpolation NI(f,vector_x);
+
+    ofstream fout("B.txt",ios::out);
+
     for(int n = 2;n<=8;n+=2){
         vector_x.clear();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
             vector_x.push_back(-5+10.0/n*i);
-        NI.change_FunctionAndPoints(f,vector_x);
-        //fout NI.get_parameters()
+        NI.change_FunctionAndPoints(f,vector_x,0);
+        NI.check_correctness();
+        parameters = NI.get_parameters();
+        fout << n << " ";
+        for(int i=0;i<parameters.size();i++)
+            fout << parameters[i] << " ";
+        fout << endl;
     }
+    fout.close();
     
     return 0;
 }
--- chapter2hw/code/Newton interpolation.h
+++ chapter2hw/code/Newton interpolation.h
@@ -5,22 +5,29 @@ using namespace std;
 
 class NewtonInterpolation{
     public:
-    NewtonInterpolation(Function &F,vector<double> &interpolating_points):interpolating_points(interpolating_points){
-        if (interpolating_points.size() == 0)
+    NewtonInterpolation(Function *F,vector<double> &interpolating_points):m_interpolating_points(interpolating_points),m_F(F){
+        if (m_interpolating_points.size() == 0)
             return;
-        Calc_divided_difference(F);
+        Calc_divided_difference();
+    }
+    ~NewtonInterpolation(){
+        delete m_F;
     }
 
-    void change_FunctionAndPoints(Function &F,vector<double> &vector_x){
-        interpolating_points = vector_x;
-        Calc_divided_difference(F);
+    void change_FunctionAndPoints(Function *F,vector<double> &vector_x,int funtion_changed=1){
+        if(funtion_changed){
+            delete m_F;
+            m_F = F;
+        }
+        m_interpolating_points = vector_x;
+        Calc_divided_difference();
     }
 
     double estimate(double x){
-        int n = interpolating_points.size()-1;
+        int n = m_interpolating_points.size()-1;
         double result = m_parameters[n];
         for(int i=n-1;i>=0;i--){
-            result *= (x-interpolating_points[i]);
+            result *= (x-m_interpolating_points[i]);
             result += m_parameters[i];
         }
         return result;
@@ -35,21 +42,36 @@ class NewtonInterpolation{
         return m_parameters;
     }
 
+    void check_correctness(){
+        int correct = 1;
+        double result;
+        for(int i=0;i<m_interpolating_points.size();i++){
+            result = estimate(m_interpolating_points[i]);
+            if (abs(result-m_F->evaluate(m_interpolating_points[i]))>1e-10){
+                cout << "Error: f(x"<<i<<") = "<<result<<" != "<<m_parameters[m_interpolating_points.size()-1]<<endl;
+                correct = 0;
+            }
+        }
+        if (correct)
+            cout << "Correct!" << endl;
+    }
+
     private:
-    void Calc_divided_difference(Function &F){
+    void Calc_divided_difference(){
         m_parameters.clear();
-        int n = interpolating_points.size()-1;
+        int n = m_interpolating_points.size()-1;
         for(int i=0;i<=n;i++)
-            m_parameters.push_back(F.evaluate(interpolating_points[i]));
+            m_parameters.push_back(m_F->evaluate(m_interpolating_points[i]));
         
         for(int i=1;i<=n;i++){
             for(int j=n;j>=i;j--){
-                m_parameters[j] = (m_parameters[j]-m_parameters[j-1])/(interpolating_points[j]-interpolating_points[j-i]);
+                m_parameters[j] = (m_parameters[j]-m_parameters[j-1])/(m_interpolating_points[j]-m_interpolating_points[j-i]);
                 //cout << "round " << i << " para["<< j <<"]"<<": " << m_parameters[j] << endl;
             }
         }
     }
 
-    vector<double> interpolating_points;
+    vector<double> m_interpolating_points;
     vector<double> m_parameters;
+    Function *m_F;
 };
