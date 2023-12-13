2efeefd4d5baa35040459e7a81feca41c5c2b411
obersturrrm <1727411859@qq.com>
2023-12-13 22:32:21 +0800

    correct A

--- chapter3hw/code/D1S32.cpp
+++ chapter3hw/code/D1S32.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    D1S32Spline spline("test_D1S32.txt");
-    
+    D1S32Spline spline("../test_D1S32.txt");
+    //fout coefficents to python
     return 0;
 }
--- chapter3hw/code/Spline.h
+++ chapter3hw/code/Spline.h
@@ -12,8 +12,8 @@ using namespace std;
  **********************************************************************/
 class Spline {
 public:
-    Spline();
-    virtual  ~Spline() = 0;
+    Spline(){}
+    ~Spline(){};
     
     //evaluate s(x)
     virtual double operator()(double x) const = 0;
@@ -25,9 +25,9 @@ public:
 class Input_Condition{
     public:
     // input condition from txt, need increasing order
-    Input_Condition();
-    Input_Condition(string filename);
-    virtual ~Input_Condition() = 0;
+    Input_Condition(){}
+    Input_Condition(string filename){}
+    ~Input_Condition(){};
 };
 
 /***********************************************************************
@@ -57,7 +57,7 @@ class Input_Condition{
 
 class D1S32Input_Condition: public Input_Condition{
     public:
-    D1S32Input_Condition();
+    D1S32Input_Condition(){}
     D1S32Input_Condition(string filename){
         ifstream fin(filename);
         fin>>N;
@@ -77,10 +77,10 @@ class D1S32Input_Condition: public Input_Condition{
         cout << "number of points N = " << N << endl;
         cout << "points:" << endl;
         for(int i=0;i<N;i++){
-            cout << "x: " << points[0][i] << "y: " << points[1][i] << endl;
+            cout << "x: " << points[0][i] << " y: " << points[1][i] << endl;
         }
     }
-    ~D1S32Input_Condition();
+    ~D1S32Input_Condition(){}
 
     // number of points
     int N;
@@ -95,7 +95,7 @@ class D1S32Input_Condition: public Input_Condition{
 
 class D1S32Spline: public Spline{
 public:
-    D1S32Spline();
+    D1S32Spline(){}
     D1S32Spline(string filename){
         input_condition = D1S32Input_Condition(filename);
         int n = 3,N = input_condition.N;
@@ -111,7 +111,7 @@ public:
         Calc_divided_difference();
         Calc_coefficients();
     }
-    ~D1S32Spline();
+    ~D1S32Spline(){}
 
     //evaluate s(x)
     double operator()(double x) const{
@@ -132,21 +132,27 @@ private:
         int N = input_condition.N;
         divided_difference[0] = input_condition.m1;
         divided_difference[N] = input_condition.mN;
+        // cout << "divided_difference[0] = m1" << divided_difference[0] << endl;
+        // cout << "divided_difference[N] = mN" << divided_difference[N] << endl;
+        
         for(int i=1;i<N;i++){
             divided_difference[i] = (input_condition.points[1][i]-input_condition.points[1][i-1])/(input_condition.points[0][i]-input_condition.points[0][i-1]);
         }
-        divided_difference[1] = (divided_difference[1]+divided_difference[0])/(input_condition.points[0][1]-input_condition.points[0][0]);
-        divided_difference[N] = (divided_difference[N]+divided_difference[N-1])/(input_condition.points[0][N]-input_condition.points[0][N-1]);
-        for(int i=N-1;i>=2;i++){
+        divided_difference[N] = (divided_difference[N]-divided_difference[N-1])/(input_condition.points[0][N-1]-input_condition.points[0][N-2]);
+        for(int i=N-1;i>=2;i--){
             divided_difference[i] = (divided_difference[i]-divided_difference[i-1])/(input_condition.points[0][i]-input_condition.points[0][i-2]);
         }
+        divided_difference[1] = (divided_difference[1]-divided_difference[0])/(input_condition.points[0][1]-input_condition.points[0][0]);
+
         //6*[1]~6*[N] is vec b
+        
     }
 
     void Calc_coefficients(){
         int N = input_condition.N;
         Eigen::Matrix<double, Eigen::Dynamic,Eigen::Dynamic> A;
-        A.resize(N,N);
+        A.setZero(N,N);
+
         A(0,0) = 2;
         A(0,1) = 1;
         A(N-1,N-1) = 2;
@@ -163,6 +169,7 @@ private:
             lambda /= input_condition.points[0][i+1]-input_condition.points[0][i-1];
             A(i,i+1) = lambda;
         }
+
         Eigen::Matrix<double, Eigen::Dynamic,1> b;
         b.resize(N,1);
         for(int i=1;i<=N;i++){
@@ -172,10 +179,10 @@ private:
         M.resize(N,1);
         M = A.colPivHouseholderQr().solve(b);
 
-        cout << "A:" << endl;
-        cout << A << endl;
-        cout << "M:" << endl;
-        cout << M << endl;
+        // cout << "A:" << endl;
+        // cout << A << endl;
+        // cout << "b:" << endl;
+        // cout << b << endl;
 
         //coefficients[i][j] means the coefficient of x^j in the ith interval,k<=n-2
         for(int i=0;i<N-1;i++){
@@ -209,8 +216,8 @@ private:
 
 class D2S32Input_Condition: public Input_Condition{
     public:
-    D2S32Input_Condition();
-    ~D2S32Input_Condition();
+    D2S32Input_Condition(){}
+    ~D2S32Input_Condition(){}
 };
 
 class D2S32Spline: public Spline{
